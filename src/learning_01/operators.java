9a199c2ae7dd95d94dc316871584fbda42f27181
Swaroop <swaroophc822@gmail.com>
2024-12-01 13:42:44 +0530

    initial commit

---
+++ src/learning_01/operators.java
@@ -0,0 +1,51 @@
+package learning_01;
+
+public class operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Adding two numbers
+		int a=100;
+		int b=200;
+		System.out.println(a+b);	
+		
+		int c =200;
+		int d =100;
+		System.out.println(c-d);
+		
+		int e= 100;
+		int f= 300;
+		System.out.println(e*f);
+		
+		int g= 900;
+		int h= 3;
+		System.out.println(g/h);
+		
+		//relational operators
+		int i=200;
+		int k=100;
+		System.out.println(i==k);
+		System.out.println(i>k);
+		System.out.println(i<k);
+		System.out.println(i<=k);
+		System.out.println(i>=k);
+		
+		//logical operators
+		boolean x=true;
+		boolean y=false;
+		System.out.println(x&&y);
+		System.out.println(x||y);
+		System.out.println(!x);
+		
+		//increment & decrement operators
+		
+		a=100;
+		a++;
+		System.out.println(a);
+		
+		b=100;
+		b--;
+		System.out.println(b);
+		
+		}
+}
