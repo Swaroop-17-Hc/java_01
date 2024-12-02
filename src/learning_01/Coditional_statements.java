949551b9083a86ab7dd1260ef11431ed40ffcf9c
Swaroop <swaroophc822@gmail.com>
2024-12-02 16:06:47 +0530

    initial

---
+++ src/learning_01/Coditional_statements.java
@@ -0,0 +1,25 @@
+package learning_01;
+
+public class Coditional_statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//if else loop statement
+		
+		int a=100;
+		int b=200;
+		
+		if(a<b) {
+			System.out.println("a is lesser than b");	
+			}
+		else if(a>b){
+			System.out.println("a is not lesser than b");
+		}
+		else {
+			System.out.println("...");
+		}
+	
+	
+	}
+
+}
